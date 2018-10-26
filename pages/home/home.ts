272663576dedafa8a621976bd6d9d97af0fedd4c
Pedro Maia <pedro.maia@ccc.ufcg.edu.br>
2018-10-26 09:33:05 -0200

    adicionando botoes

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public alerCtrl: AlertController) {
 
   }
 
+  mostraAlertOla() {
+    let alert = this.alerCtrl.create({
+      title: 'Olá',
+      message: 'Tudo bem?',
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
+  mostraAlertMassa() {
+    let alert = this.alerCtrl.create({
+      title: 'Massa',
+      message: 'Peso',
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
+  mostraAlertShow() {
+    let alert = this.alerCtrl.create({
+      title: 'Show!',
+      message: 'Mesmo!',
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
 }
