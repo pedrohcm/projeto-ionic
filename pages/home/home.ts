980b81be285acf6d79be67f49effe6fdf282b8eb
Lucas Oliveira <lucas.oliveira@ccc.ufcg.edu.br>
2018-10-26 21:57:17 -0300

    Adicionando alert na Home

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  testRadioOpen = true;
+  testRadioResult = null;
 
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+  }
+
+  showRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Lightsaber color');
+    alert.addInput({ type: 'radio', label: 'Blue', value: 'blue', checked: true });
+    alert.addButton('Cancel');
+    alert.addButton({ text: 'OK', handler: data => { this.testRadioOpen = false; this.testRadioResult = data; } });
+    alert.present();
   }
 
 }
